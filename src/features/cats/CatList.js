af39189c55947100b2a3b14bd0190549560b147a
Brandon Castro <castroyou999@gmail.com>
2023-03-11 21:48:06 -0500

    finished test

--- src/features/cats/CatList.js
+++ src/features/cats/CatList.js
@@ -1,1 +1,11 @@
 // write your CatList component here
+
+export default function CatList({catPics}){
+    const allCatPics = catPics.map(cat => (<img key={cat.id} src={cat.url} alt="cat" />) )
+
+    return(
+        <div>
+            {allCatPics}
+        </div>
+    )
+}
